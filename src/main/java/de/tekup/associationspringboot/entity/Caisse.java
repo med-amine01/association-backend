1325d0b28170b8ad7660c63e0cf21c318d87b4da
ab-mokhtar <mokhtarabbes9@gmail.com>
2023-04-10 10:15:05 +0000

    Entity caisse Singleton

---
+++ src/main/java/de/tekup/associationspringboot/entity/Caisse.java
@@ -0,0 +1,42 @@
+package de.tekup.associationspringboot.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Caisse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double amount;
+    private static final Caisse instance=new Caisse();
+
+    public static Caisse getInstance() {
+        return instance;
+    }
+
+    public synchronized void ajouter(double montant) {
+        amount += montant;
+    }
+
+    public synchronized void retirer(double montant) {
+        amount -= montant;
+    }
+
+    public synchronized double getSolde() {
+        return amount;
+    }
+
+
+
+    // getters, setters, etc.
+
+
+}
